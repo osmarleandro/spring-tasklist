510655b88b31f1cddcdca2c5f074abebac5658ed
Osmar Leandro <osmarleandrods@gmail.com>
2023-10-15 11:11:44 -0300

    fix: validate task dates

--- src/main/java/br/com/olds/todolist/task/TaskController.java
+++ src/main/java/br/com/olds/todolist/task/TaskController.java
@@ -1,5 +1,6 @@
 package br.com.olds.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +21,21 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public ResponseEntity<TaskModel> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity<Object> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         UUID userId = (UUID) request.getAttribute("idUser");
         taskModel.setIdUser(userId);
+
+        var currentDate = LocalDateTime.now();
+
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início / data de término deve ser maior que a data atual");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início deve ser menor que a data de término");
+        }
 
         var task = taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(task);
