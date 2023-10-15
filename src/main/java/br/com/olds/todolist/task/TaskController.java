19e035dd600a26e752d8a2a32e541381e5c16aaa
Osmar Leandro <osmarleandrods@gmail.com>
2023-10-15 11:15:06 -0300

    feat: list all tasks by user

--- src/main/java/br/com/olds/todolist/task/TaskController.java
+++ src/main/java/br/com/olds/todolist/task/TaskController.java
@@ -1,11 +1,13 @@
 package br.com.olds.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +41,14 @@ public class TaskController {
 
         var task = taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+
+        return tasks;
     }
 
 }
