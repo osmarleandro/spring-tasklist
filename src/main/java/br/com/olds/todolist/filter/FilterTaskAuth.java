f45f0dc32dfa75ba26906278b567f1e68cbcb27d
Osmar Leandro <osmarleandrods@gmail.com>
2023-10-15 11:21:16 -0300

    fix: filter routes by start with

--- src/main/java/br/com/olds/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/olds/todolist/filter/FilterTaskAuth.java
@@ -26,7 +26,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
         var servletPath = request.getServletPath();
 
-        if (!servletPath.equals("/tasks")) {
+        if (!servletPath.startsWith("/tasks")) {
             filterChain.doFilter(request, response);
         }
 
